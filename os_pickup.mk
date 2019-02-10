84f48694df6d1c5ddcb441d572d5b72146ff1123
Roger Liao <rogerliao@google.com>
2019-02-10 16:59:21 +0800

    Include Android.mk when building B4S4
    [b1c1/b4s4 share codebase]
    Include all Android.mk under hardware/qcom/sdm845 when building B4S4
    
    Bug: 123477980
    Change-Id: Id78ba3fee5e7a51036fac197401b7c51c9762d65

--- os_pickup.mk
+++ os_pickup.mk
@@ -1,4 +1,4 @@
-ifeq ($(TARGET_BOARD_PLATFORM),sdm845)
+ifneq ($(filter sdm845 sdm710,$(TARGET_BOARD_PLATFORM)),)
 LOCAL_PATH := $(call my-dir)
 include $(call first-makefiles-under,$(LOCAL_PATH))
 endif
