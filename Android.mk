a5a8facfffe8fcf50e9b43644f77071e6b0d8138
Michael Bestas <mkbestas@lineageos.org>
2024-10-14 00:17:41 +0300

    Move sdm845 files to the top folder
    Change-Id: Ifabfe600bbc47aca4c58e367b39156c34905d625

--- Android.mk
+++ Android.mk
@@ -1,7 +1,6 @@
-# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
-ifneq ($(BOARD_IS_AUTOMOTIVE),true)
-  ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-    LOCAL_PATH := $(call my-dir)
-    include $(call all-named-subdir-makefiles,sdm845)
-  endif #BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
+ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
+LOCAL_PATH := $(call my-dir)
+include $(LOCAL_PATH)/build/target_specific_features.mk
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
